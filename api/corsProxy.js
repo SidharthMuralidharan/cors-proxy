f5cf88592d5160a2838bbdac8a9e85385d6e7a7c
Sidharth Muralidharan <sidmuralidharan@gmail.com>
2023-10-09 15:15:35 +0530

    Update corsProxy.js
    console.logs

--- api/corsProxy.js
+++ api/corsProxy.js
@@ -3,11 +3,11 @@ const axios = require('axios');
 export default async (req, res) => {
   const url = req.query.url;
 
-  try {
-    const response = await axios.get(url);
-    res.status(200).json(response.data);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Failed to fetch data' });
-  }
-};
+try {
+  const response = await axios.get(url);
+  console.log('Success: ', response.data);
+  res.status(200).json(response.data);
+} catch (error) {
+  console.error('Error: ', error);
+  res.status(500).json({ error: 'Failed to fetch data' });
+}
